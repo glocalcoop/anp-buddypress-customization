b387d86c6c208ceb4cec33f6fc142af72dd3945c
Pea Lutz <pea@glocal.coop>
2016-03-11 19:27:18 -0500

    Added function to change the display of BuddyPress tab for forums Added function to change slug for  forums

--- inc/bbpress-custom-functions.php
+++ inc/bbpress-custom-functions.php
@@ -10,16 +10,13 @@
  * @package   ANP_BuddyPress_Customization
  */
 
-
-
-/* 
- * the_title()
- * Modify the title to display the meeting type and meeting date rather than post title
+/**
+ * Change the labels registered for bbPress forum post type
+ * Note: This only affects the dashboard and native bbPress, not display in BuddyPress
  */
-
 if( !function_exists( 'anp_rename_forums' ) ) {
 
-    function anp_rename_forums($array) {
+    function anp_rename_forums( $array ) {
 
         $array = array(
             'name'               => __( 'Discussion', ANP_BP_CUSTOM_PLUGIN_NAMESPACE ),
@@ -46,5 +43,146 @@ if( !function_exists( 'anp_rename_forums' ) ) {
 add_filter( 'bbp_get_forum_post_type_labels', 'anp_rename_forums' );
 
 }
+
+/**
+ * Change Forum and Forums text
+ * Replace Forum and Forums with Discussion and Discussions respectively
+ * @link {https://bbpress.org/forums/topic/group-forum-tab/}
+ */
+if ( ! function_exists( 'anp_change_forums_text' ) ) {
+
+    function anp_change_forums_text( $translated_text ) {
+        if ( $translated_text == 'Forum' ) {
+            $translated_text = 'Discussion';
+        } elseif( $translated_text == 'Forums' ) {
+            $translated_text = 'Discussions';
+        }
+        return $translated_text;
+    }
+
+add_filter( 'gettext', 'anp_change_forums_text', 20 );
+}
+
+
+/**
+ * Hide Topic Menu
+ * Hide the Topics menu in the dashboard
+ */
+if( !function_exists( 'anp_hide_topic_admin_menu' ) ) {
+
+    function anp_hide_topic_admin_menu( $array ) {
+
+        $array = array(
+            'labels'              => bbp_get_topic_post_type_labels(),
+            'rewrite'             => bbp_get_topic_post_type_rewrite(),
+            'supports'            => bbp_get_topic_post_type_supports(),
+            'description'         => __( 'Topics', ANP_BP_CUSTOM_PLUGIN_NAMESPACE ),
+            'capabilities'        => bbp_get_topic_caps(),
+            'capability_type'     => array( 'topic', 'topics' ),
+            'menu_position'       => 555555,
+            'has_archive'         => ( 'discussion' === bbp_show_on_root() ) ? bbp_get_topic_archive_slug() : false,
+            'exclude_from_search' => true,
+            'show_in_nav_menus'   => false,
+            'public'              => true,
+            'show_ui'             => current_user_can( 'bbp_topics_admin' ),
+            'can_export'          => true,
+            'hierarchical'        => false,
+            'query_var'           => true,
+            'menu_icon'           => '',
+            'show_in_menu'        => false
+        );
+
+        return $array;
+
+    }
+
+add_filter( 'bbp_register_topic_post_type', 'anp_hide_topic_admin_menu' );
+
+}
+
+/**
+ * Hide Reply Menu
+ * Hide the Replies menu in the dashboard
+ */
+if( !function_exists( 'anp_hide_reply_admin_menu' ) ) {
+
+    function anp_hide_reply_admin_menu( $array ) {
+
+        $array = array(
+            'labels'              => bbp_get_reply_post_type_labels(),
+            'rewrite'             => bbp_get_reply_post_type_rewrite(),
+            'supports'            => bbp_get_reply_post_type_supports(),
+            'description'         => __( 'Topic Replies', ANP_BP_CUSTOM_PLUGIN_NAMESPACE ),
+            'capabilities'        => bbp_get_reply_caps(),
+            'capability_type'     => array( 'reply', 'replies' ),
+            'menu_position'       => 555555,
+            'exclude_from_search' => true,
+            'has_archive'         => false,
+            'show_in_nav_menus'   => false,
+            'public'              => true,
+            'show_ui'             => current_user_can( 'bbp_replies_admin' ),
+            'can_export'          => true,
+            'hierarchical'        => false,
+            'query_var'           => true,
+            'menu_icon'           => '',
+            'show_in_menu'        => false
+        );
+
+        return $array;
+
+    }
+
+add_filter( 'bbp_register_reply_post_type', 'anp_hide_reply_admin_menu' );
+
+}
+
+/**
+ * Add Topics and Replies as Submenus of Discussions
+ * Show the menu items under the Discussions menu
+ */
+if ( ! function_exists( 'anp_add_subitems_to_forums' ) ) {
+
+    function anp_add_subitems_to_forums() { 
+
+        //edit.php?post_type=forum
+
+        add_submenu_page(
+            'edit.php?post_type=forum', 
+            __( 'All Topics', ANP_BP_CUSTOM_PLUGIN_NAMESPACE ), 
+            __( 'All Topics', ANP_BP_CUSTOM_PLUGIN_NAMESPACE ), 
+            'manage_options', 
+            'edit.php?post_type=topic'
+        ); 
+
+        add_submenu_page(
+            'edit.php?post_type=forum', 
+            __( 'New Topic', ANP_BP_CUSTOM_PLUGIN_NAMESPACE ), 
+            __( 'New Topic', ANP_BP_CUSTOM_PLUGIN_NAMESPACE ), 
+            'manage_options', 
+            'post-new.php?post_type=topic'
+        ); 
+
+        add_submenu_page(
+            'edit.php?post_type=forum', 
+            __( 'All Replies', ANP_BP_CUSTOM_PLUGIN_NAMESPACE ), 
+            __( 'All Replies', ANP_BP_CUSTOM_PLUGIN_NAMESPACE ), 
+            'manage_options', 
+            'post-new.php?post_type=reply'
+        ); 
+
+        add_submenu_page(
+            'edit.php?post_type=forum', 
+            __( 'New Reply', ANP_BP_CUSTOM_PLUGIN_NAMESPACE ), 
+            __( 'New Reply', ANP_BP_CUSTOM_PLUGIN_NAMESPACE ), 
+            'manage_options', 
+            'post-new.php?post_type=reply'
+        ); 
+
+    }
+
+    add_action( 'admin_menu', 'anp_add_subitems_to_forums' ); 
+
+}
+
 
 ?>
