1e5a702dcfc3924e4a4e2e1e5613e7af91a01424
Pea Lutz <pea@glocal.coop>
2016-08-28 14:12:49 -0400

    [Task #1551] Moved BBPress customization from BuddyPress Customization plugin to Core Functionality

--- anp-buddypress-customization.php
+++ anp-buddypress-customization.php
@@ -6,7 +6,7 @@
  * @author    Pea, Glocal
  * @license   GPL-2.0+
  * @link      http://glocal.coop
- * @since     1.0.0
+ * @since     1.0.1
  * @package   ANP_BuddyPress_Customization
  */
 
@@ -15,7 +15,7 @@ Plugin Name: Activist Network BuddyPress Customization
 Description: Customization for BuddyPress and associated plugins.
 Author: Pea, Glocal
 Author URI: http://glocal.coop
-Version: 1.0
+Version: 1.0.1
 License: GPLv3
 Text Domain: anp-bp-custom
 */
@@ -25,30 +25,21 @@ if ( ! defined( 'WPINC' ) ) {
     die;
 }
 
-
-/* ---------------------------------- *
+/**
  * Constants
- * ---------------------------------- */
-
+ */
 if ( !defined( 'ANP_BP_CUSTOM_PLUGIN_DIR' ) ) {
     define( 'ANP_BP_CUSTOM_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 }
-
 if ( !defined( 'ANP_BP_CUSTOM_PLUGIN_URL' ) ) {
     define( 'ANP_BP_CUSTOM_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 }
 
-
-/* ---------------------------------- *
+/**
  * Required Files
- * ---------------------------------- */
-
-include_once( ANP_BP_CUSTOM_PLUGIN_DIR . '/inc/bbpress-custom-functions.php' );
+ */
 include_once( ANP_BP_CUSTOM_PLUGIN_DIR . '/inc/buddypress-docs-custom-functions.php' );
 include_once( ANP_BP_CUSTOM_PLUGIN_DIR . '/inc/bp-groupblog-custom-functions.php' );
 include_once( ANP_BP_CUSTOM_PLUGIN_DIR . '/inc/invite-anyone-custom-functions.php' );
 include_once( ANP_BP_CUSTOM_PLUGIN_DIR . '/inc/group-subscriptions-custom-functions.php' );
 include_once( ANP_BP_CUSTOM_PLUGIN_DIR . '/inc/friendship-custom-functions.php' );
-
-
-?>
--- readme.txt
+++ readme.txt
@@ -1,6 +1,33 @@
 === Activist Network BuddyPress Customization ===
-Contributors: Pea, Glocal
+Contributors: misfist
 Tags: buddypress, community, social
-License: GPL3
+Requires at least: 3.0.1
+Tested up to: 4.5
+Stable tag: beta
+Version: 1.0.1
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
 Customization for BuddyPress and associated plugins.
+
+== Description ==
+
+Customization for BuddyPress and associated plugins.
+
+== Installation ==
+
+This section describes how to install the plugin and get it working.
+
+e.g.
+
+1. Upload `anp-buddypress-customization.php` to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+1. Place `<?php do_action( 'plugin_name_hook' ); ?>` in your templates
+
+== Changelog ==
+
+= 1.0.1 August 28, 2016 =
+[Task #1551] Moved BBPress customization from BuddyPress Customization plugin to Core Functionality
+
+= 1.0.0 =
+Initial release
